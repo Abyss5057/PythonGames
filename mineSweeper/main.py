e81d37faf2a7daf106aacaef55f92a9a7d540f4e
Abyss5057 <417keikunv2@gmail.com>
2024-06-06 14:47:44 +0900

    ms 1

--- mineSweeper/main.py
+++ mineSweeper/main.py
@@ -3,26 +3,54 @@ import random as rd
 # 一気に壊れる挙動が理解できない
 # 調べる必要あり
 
-def drow(board, mask, mes = ""):
-    """
-    <board>
-    0 = empty
-    1 = bomb
+def createBoard(height, width):
+    board = []
+    for i in range(height):
+        board.append([0] * width)
+    
+    return board
 
-    board = [
-        [0, 0, 0, 0, 1, 0, 0, 0],
-        [1, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 1]
-    ]
+def placeBomb(board, num):
+    height = len(board)
+    width = len(board[0])
 
+    if(height * width < num):
+        print("The number of bomb is larger than board!")
+        return
 
-    <mask>
-    0 = unmasked
-    1 = masked
+    bombPos = []
 
-    mask = [
-        [0, 0, 0, 0, 1, 0, 0, 0],
-        [1, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 1]
-    ]
-    """
+    for i in range(num):
+        x = rd.randint(0, width - 1)
+        y = rd.randint(0, height - 1)
+
+        while([x, y] in bombPos):
+            x = rd.randint(0, width - 1)
+            y = rd.randint(0, height - 1)
+        
+        bombPos.append([x, y])
+        board[y][x] = "b"
+    
+    return board
+
+def placeIndicater(board):
+    pass
+
+def drow(board, mask = [], mes = ""):
+    isHideZero = True
+
+    height = len(board)
+    width = len(board[0])
+
+    for i in range(height):
+        line = ""
+        for j in range(width):
+            line += str(board[i][j])
+        
+        if(isHideZero):
+            line = line.replace("0", " ")
+        
+        print(line)
+    
+
+drow(placeBomb(createBoard(4, 8), 5))
