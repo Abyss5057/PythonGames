d0dbf759674f3fc56b39d01da18ecb027cea953a
Abyss5057 <417keikunv2@gmail.com>
2024-06-12 12:44:31 +0900

    as

--- yokoSuku/main.py
+++ yokoSuku/main.py
@@ -8,5 +8,13 @@ pref_screen_y = 10
 pref_key_up = "w"
 pref_key_down = "s"
 
-def drow():
+# ---Internal Values---
+# DONT TOUCH
+
+seq = 0
+
+def drowBar(seq, length):
+    pass
+
+def drowFrame():
     pass
