4512147425d6c7bf9b697247c4426500222a41f6
Abyss5057 <417keikunv2@gmail.com>
2025-06-21 15:25:37 +0900

    Revert "as"
    This reverts commit d0dbf759674f3fc56b39d01da18ecb027cea953a.

--- yokoSuku/main.py
+++ yokoSuku/main.py
@@ -8,13 +8,5 @@ pref_screen_y = 10
 pref_key_up = "w"
 pref_key_down = "s"
 
-# ---Internal Values---
-# DONT TOUCH
-
-seq = 0
-
-def drowBar(seq, length):
-    pass
-
-def drowFrame():
+def drow():
     pass
